690e82dbd602c77ea0c6a268c90e78caa2155192
Brian Haas <brianjohnhaas@gmail.com>
2015-08-01 22:02:11 -0400

    instead capturing discordant paris

--- util/capture_discordant_pairs_from_fastq.pl
+++ util/capture_discordant_pairs_from_fastq.pl
@@ -9,6 +9,10 @@ use SAM_reader;
 use SAM_entry;
 use Fastq_reader;
 
+
+my $MIN_MAPPING_QUALITY = 10;
+
+
 my $usage = "usage: $0 left.fq right.fq aligned.bam out_prefix\n\n";
 
 
@@ -20,7 +24,7 @@ my $out_prefix = $ARGV[3] or die $usage;
 
 main: {
 
-    my %proper_pairs;
+    my %fastq_reads_capture;
 
     my $c = 0;
     my $sam_reader = new SAM_reader($aligned_bam);
@@ -30,21 +34,24 @@ main: {
         if ($c % 10000 == 0) {
             print STDERR "\r[$c]   ";
         }
-        if ($sam_entry->is_proper_pair()) {
+        if ( (! $sam_entry->is_query_unmapped() )
+             && 
+             $sam_entry->get_mapping_quality() >= $MIN_MAPPING_QUALITY
+             &&
+             $sam_entry->is_mate_unmapped()
+            ) {
             my $read_name = $sam_entry->get_read_name();
-            $proper_pairs{$read_name}++;
+            $fastq_reads_capture{$read_name}++;
         }
     }
     
-    my $num_proper_pairs = scalar(keys %proper_pairs);
-    print STDERR "$0 : $num_proper_pairs proper pairs identified.\n";
-    
+    my $num_reads_capture = scalar(keys %fastq_reads_capture);
+    print STDERR "$0 : $num_reads_capture read pairs to capture as discordant.\n";
     
     my $new_left_fq = "${out_prefix}_1.fastq";
     my $new_right_fq = "${out_prefix}_2.fastq";
     
-
-
+    
     foreach my $file_pair ( [$left_fq, $new_left_fq],
                             [$right_fq, $new_right_fq] ) {
 
@@ -61,7 +68,7 @@ main: {
 
             my $core_read_name = $fq_obj->get_core_read_name();
             
-            unless (exists $proper_pairs{$core_read_name}) {
+            if (exists $fastq_reads_capture{$core_read_name}) {
                 
                 print $ofh $fq_obj->get_fastq_record();
             
@@ -72,7 +79,7 @@ main: {
         print STDERR "$0 : $in_file : $count_captured_reads num_captured_reads\n";
         
         close $ofh;
-
+        
 
         my $cmd = "gzip $out_file";
         my $ret = system($cmd);
