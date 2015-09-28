c7edead237ab7488511d34298b0f07b35dfbbdc0
Brian Haas <brianjohnhaas@gmail.com>
2015-09-28 13:48:34 -0400

    filter based on frag support

--- util/BLASTN.filter.pl
+++ util/BLASTN.filter.pl
@@ -30,6 +30,11 @@ my $usage = <<__EOUSAGE__;
 #  --fusion_preds <string>        preliminary fusion prediction (file: finspector.fusion_preds.coalesced.summary)s
 #
 #  --out_prefix <string>          output prefix for STAR-Fusion.filter  (adds .final and .final.abridged as outputs)
+#  --max_promiscuity <int>               maximum number of partners allowed for a given fusion. Default: 3
+#  --ref_cdna <string>            reference cDNA sequences fasta file 
+#  -E <float>                     E-value threshold for blast searches (default: 0.001)
+#
+#
 #
 ########################################################################  
 
@@ -43,11 +48,17 @@ my $help_flag;
 my $fusion_preds_file;
 my $out_prefix;
 
+my $Evalue = 1e-3;
+my $max_promiscuity = 3;
+my $ref_cdna;
 
 &GetOptions ( 'h' => \$help_flag, 
               
               'fusion_preds=s' => \$fusion_preds_file,
               'out_prefix=s' => \$out_prefix,
+              'E=f' => \$Evalue,
+              'max_promiscuity=i' => \$max_promiscuity,
+              'ref_cdna=s' => \$ref_cdna,
     );
 
 
@@ -55,7 +66,7 @@ if ($help_flag) {
     die $usage;
 }
 
-unless ($fusion_preds_file) {
+unless ($fusion_preds_file && $out_prefix && defined($Evalue) && $max_promiscuity && $ref_cdna) {
     die $usage;
 }
 
@@ -129,7 +140,15 @@ main: {
     
     ## now do the homology filter
     
-    my $cmd = "$STAR_FUSION_DIR/util/blast_and_promiscuity_filter.pl --fusion_preds $star_fusion_fmt_file  --out_prefix $out_prefix";
+    my $cmd = "$STAR_FUSION_DIR/util/blast_and_promiscuity_filter.pl " 
+        . " --fusion_preds $star_fusion_fmt_file  "
+        . " --max_promiscuity $max_promiscuity "
+        . " --out_prefix $out_prefix "
+        . " -E $Evalue "
+        . " --ref_cdna $ref_cdna ";
+    
+    
+
     &process_cmd($cmd);
 
 }
---
+++ util/filter_fusions_by_frag_thresholds.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Carp;
+use Getopt::Long qw(:config posix_default no_ignore_case bundling pass_through);
+
+
+my $usage = <<__EOUSAGE__;
+
+#########################################################################################################
+#
+#  --min_junction_reads <int>            minimum number of junction-spanning reads required
+#
+#  --min_sum_frags <int>                 minimum fusion support = ( # junction_reads + # spanning_frags )
+#
+#  --min_novel_junction_support <int>    minimum number of junction reads required if breakpoint 
+#                                          lacks involvement of only reference junctions
+#
+#  --fusion_preds <string>               fusion predictions in star-F-ftm
+#
+#########################################################################################################
+
+
+
+__EOUSAGE__
+
+    ;
+
+
+
+my $help_flag;
+
+my $min_junction_reads;
+my $min_sum_frags;
+my $min_novel_junction_support;
+my $fusion_preds;
+
+&GetOptions ( 'h' => \$help_flag,
+              'min_junction_reads=i' => \$min_junction_reads,
+              'min_sum_frags=i' => \$min_sum_frags,
+              'min_novel_junction_support=i' => \$min_novel_junction_support,
+              'fusion_preds=s' => \$fusion_preds,
+    );
+
+
+
+foreach my $var ($min_junction_reads, $min_sum_frags, $min_novel_junction_support, $fusion_preds) {
+    unless (defined $var) {
+        die $usage;
+    }
+}
+
+
+main: {
+    
+    
+    open (my $fh, $fusion_preds) or die $!;
+    while (<$fh>) {
+        my $line = $_;
+        if (/^\#/) { 
+            print $line;
+            next;
+        }
+
+        my @x = split(/\t/, $line);
+        my $J = $x[1];
+        my $S = $x[2];
+        my $splice_type = $x[3];
+
+        if ($splice_type ne 'ONLY_REF_SPLICE') {
+            unless ($J >= $min_novel_junction_support) {
+                next;
+            }
+        }
+
+        my $sum_JS = $J + $S;
+        
+        if ($J >= $min_junction_reads && $sum_JS >= $min_sum_frags) {
+
+            print $line;
+        }
+    }
+    close $fh;
+
+    
+    exit(0);
+}
+
+
+        
