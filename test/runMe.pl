6001f26e619d879a40d9f686751a2e041f23a7f2
Brian Haas <brianjohnhaas@gmail.com>
2015-10-05 09:18:50 -0400

    add

--- test/runMe.pl
+++ test/runMe.pl
@@ -8,9 +8,10 @@ use lib ("$FindBin::Bin/../PerlLib");
 use Pipeliner;
 use __GLOBALS__;
 
-my $usage = "usage: $0 align_method=(STAR|HISAT|GSNAP)\n\n";
+my $usage = "usage: $0 align_method=(STAR|HISAT|GSNAP) [INCLUDE_TRINITY_FLAG]\n\n";
 
 my $method = $ARGV[0] or die $usage;
+my $INCLUDE_TRINITY_FLAG = $ARGV[1] || 0;
 
 unless ($method =~ /GSNAP|HISAT|STAR/) {
     die $usage;
@@ -37,7 +38,7 @@ main: {
     
     my $cmd = "$INSTALL_DIR/FusionInspector --fusions test_fusions.list,test_fusions.list2,test_fusions.list3 --genome_lib $ENV{CTAT_GENOME_LIB} --left_fq $left_fq --right $right_fq --out_dir Fusion_Inspector/ --out_prefix finspector --include_whole_genome --align_utils $method ";
     
-    if (@ARGV) {
+    if ($INCLUDE_TRINITY_FLAG) {
         $cmd .= " --include_Trinity"
     }
 
