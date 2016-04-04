c86f42a01ca721e83689db767cb591d02cd2dcab
Brian Haas <brianjohnhaas@gmail.com>
2016-04-04 16:19:54 -0400

    integrating coding effect into main report

--- util/fusion_to_coding_region_effect.pl
+++ util/fusion_to_coding_region_effect.pl
@@ -46,18 +46,31 @@ main: {
 
     open (my $fh, $fusions_file) or die "Error, cannot open file $fusions_file";
     my $header = <$fh>;
+    
     unless ($header =~ /^\#fusion_name/) {
         die "Error, fusion_file: $fusions_file has unrecognizable header: $header";
     }
-    print $header;
-
+    chomp $header;
+    print $header . "\t" . join("\t", 
+                                "CDS_LEFT_ID", 
+                                "CDS_LEFT_RANGE",
+                                "CDS_RIGHT_ID",
+                                "CDS_RIGHT_RANGE",
+                                "PROT_FUSION_TYPE",
+                                "FUSION_MODEL",
+                                "FUSION_CDS",
+                                "FUSION_TRANSL",
+                                "PFAM_LEFT",
+                                "PFAM_RIGHT",
+        ) . "\n";
+        
     
     my $tied_hash = new TiedHash( { 'use' => $prot_info_db } );
 
     while (<$fh>) {
-        
+        chomp;        
         my $line = $_;
-        chomp;
+
         my @x = split(/\t/);
 
         my $fusion_name = $x[0];
@@ -66,18 +79,35 @@ main: {
                 
         my $gene_right = $x[8];
         my $break_right = $x[9];
-
-        my @results = &examine_fusion_coding_effect($gene_left, $break_left, $gene_right, $break_right, $tied_hash);
         
-        foreach my $result (@results) {
-            print join("\t", $fusion_name, 
-                       $result->{cds_left_id}, $result->{cds_left_range},
-                       $result->{cds_right_id}, $result->{cds_right_range},
-                       $result->{prot_fusion_type},
-                       $result->{fusion_coding_descr},
-                       $result->{cds_fusion_seq},
-                       $result->{prot_fusion_seq} ) . "\n";
+        my @results = &examine_fusion_coding_effect($gene_left, $break_left, $gene_right, $break_right, $tied_hash);
+
+        if (@results) {
+            ## just take the single 'best' one, arbitrarily chosen as the one with the longest fusion sequence.
+            @results = reverse sort { length($a->{prot_fusion_seq}) <=> length($b->{prot_fusion_seq}) } @results;
+            
+            #my $result = shift @results;
+            
+            foreach my $result (@results) {
+                print join("\t", #"CODING_EFFECT:", $fusion_name, 
+                           $line,
+                           $result->{cds_left_id}, $result->{cds_left_range},
+                           $result->{cds_right_id}, $result->{cds_right_range},
+                           $result->{prot_fusion_type},
+                           $result->{fusion_coding_descr},
+                           $result->{cds_fusion_seq},
+                           $result->{prot_fusion_seq},
+                           
+                           $result->{left_domains},
+                           $result->{right_domains}
+                           
+                    ) . "\n";
+            }
         }
+        else {
+            print "$line" . ("\t." x 10) . "\n";
+        }
+
     }
     close $fh;
     
@@ -98,7 +128,7 @@ sub examine_fusion_coding_effect {
     my $gene_left_json = $tied_hash->get_value($gene_left);    
     my $gene_left_aref = $coder->decode($gene_left_json);
 
-    print Dumper($gene_left_aref);
+    #print Dumper($gene_left_aref);
     #print "Gene_left: $gene_left_json\n";
     
     
@@ -141,10 +171,17 @@ sub examine_fusion_coding_effect {
                 my $fusion_seq = join("", lc($left_cds_part), uc($right_cds_part));
                 my $pep = translate_sequence($fusion_seq, 1);
 
-                my $prot_fusion_type = ( ($left_end_phase + 1) % 3 == $right_beg_phase) ? "INFRAME" : "FRAMESHIFT";
+                my $prot_fusion_type = "NA";
+                if ($left_end_phase ne '.' && $right_beg_phase ne '.') {
+                    $prot_fusion_type = ( ($left_end_phase + 1) % 3 == $right_beg_phase) ? "INFRAME" : "FRAMESHIFT";
+                }
                 
                 my $left_segs_string = &segments_to_string(@$left_fuse_segments_aref);
                 my $right_segs_string = &segments_to_string(@$right_fuse_segments_aref);
+
+                my $left_domains_string = &get_pfam_domains($cds_left_obj, $left_rel_rend, "left") || ".";
+                my $right_domains_string = &get_pfam_domains($cds_right_obj, $right_rel_lend, "right") || ".";
+                
                 
                 push (@results, { cds_left_id => $cds_left_id,
                                   cds_right_id => $cds_right_id,
@@ -154,6 +191,9 @@ sub examine_fusion_coding_effect {
                                   cds_fusion_seq => $fusion_seq,
                                   prot_fusion_seq => $pep,
                                   fusion_coding_descr => join("<==>", $left_segs_string, $right_segs_string),
+                                  left_domains => $left_domains_string,
+                                  right_domains => $right_domains_string,
+                                  
                       }
                     );
                 
@@ -192,6 +232,11 @@ sub get_left_fusion_partner_segments {
     
     my ($chr, $breakpoint_coord, $orient) = split(/:/, $breakpoint_info);
     
+    # ensure breakpoint overlaps a coding segment
+    unless (&breakpoint_overlaps_cds_segment($cds_obj, $breakpoint_coord)) {
+        return();
+    }
+    
     my ($left_segs_aref, $right_segs_aref) = &split_cds_at_breakpoint($cds_obj, $breakpoint_coord);
 
     if ($orient eq '+') {
@@ -207,6 +252,11 @@ sub get_right_fusion_partner_segments {
     my ($cds_obj, $breakpoint_info) = @_;
     
     my ($chr, $breakpoint_coord, $orient) = split(/:/, $breakpoint_info);
+
+    # ensure breakpoint overlaps a coding segment
+    unless (&breakpoint_overlaps_cds_segment($cds_obj, $breakpoint_coord)) {
+        return();
+    }
     
     my ($left_segs_aref, $right_segs_aref) = &split_cds_at_breakpoint($cds_obj, $breakpoint_coord);
     
@@ -218,6 +268,22 @@ sub get_right_fusion_partner_segments {
     }
 }
 
+
+
+####
+sub breakpoint_overlaps_cds_segment {
+    my ($cds_obj, $breakpoint_coord) = @_;
+
+    my @segments = sort {$a->{lend}<=>$b->{lend}} @{$cds_obj->{phased_segments}};
+
+    foreach my $segment (@segments) {
+        if ($segment->{lend} <= $breakpoint_coord && $breakpoint_coord <= $segment->{rend}) {
+            return(1);
+        }
+    }
+
+    return(0); # no overlap
+}
 
 
 ####
@@ -331,4 +397,89 @@ sub segments_to_string {
 
     return($descr_text);
     
+}
+
+
+####
+sub get_pfam_domains {
+    my ($cds_obj, $cds_coord, $left_or_right_side) = @_;
+
+    my @pfam_hits = @{$cds_obj->{pfam_hits}};
+
+    my @pfam_domains_selected;
+
+    foreach my $pfam_hit (@pfam_hits) {
+        my ($start, $end) = ($pfam_hit->{query_start}, $pfam_hit->{query_end});
+
+        if  (($left_or_right_side eq 'left' && $end <= $cds_coord)
+             ||
+             ($left_or_right_side eq 'right' && $start >= $cds_coord) ) {
+
+            ## domain entirely on the side of the protein included in the fusion.
+            
+            push (@pfam_domains_selected, $pfam_hit);
+        }
+        elsif ($start < $cds_coord && $cds_coord < $end) {
+            ## overlaps
+
+            ## fragment it and return the fragment.
+            my $pfam_hit_copy = &clone($pfam_hit);
+            if ($left_or_right_side eq 'left') {
+                $pfam_hit_copy->{query_end} = $cds_coord;
+                $pfam_hit_copy->{query_end_partial} = 1;
+            }
+            else {
+                # right side
+                $pfam_hit_copy->{query_start} = $cds_coord;
+                $pfam_hit_copy->{query_start_partial} = 1;
+            }
+            $pfam_hit_copy->{hmmer_domain} .= "-PARTIAL";
+            push (@pfam_domains_selected, $pfam_hit_copy);
+            
+        }
+    }
+
+    ## generate a summary string.
+    @pfam_domains_selected = sort {$a->{query_start}<=>$b->{query_start}} @pfam_domains_selected;
+
+    my @pfam_descrs;
+    foreach my $pfam_domain (@pfam_domains_selected) {
+        #'query_start' => '369',
+        #'cds_id' => 'DISP1|ENST00000284476.6',
+        #'domain_evalue' => '7.3e-21',
+        #'query_end' => '733',
+        #'hmmer_domain' => 'Patched'
+
+        if ($pfam_domain->{query_start_partial}) {
+            $pfam_domain->{query_start} = "~" . $pfam_domain->{query_start};
+        }
+        if ($pfam_domain->{query_end_partial}) {
+            $pfam_domain->{query_end} = $pfam_domain->{query_end} . "~";
+        }
+        
+        my $descr = join("|", $pfam_domain->{hmmer_domain},
+                         $pfam_domain->{query_start} . "-" . $pfam_domain->{query_end},
+                         $pfam_domain->{domain_evalue});
+
+        push (@pfam_descrs, $descr);
+    }
+
+    my $ret_descr = join("^", @pfam_descrs);
+
+    return($ret_descr);
+            
+}
+
+
+####
+sub clone {
+    my ($hashref) = @_;
+
+    my $clone_ref = {};
+    foreach my $key (keys %$hashref) {
+
+        $clone_ref->{$key} = $hashref->{$key};
+    }
+
+    return($clone_ref);
 }
